bb79be0a8c855be383f39d0636a76b8f6cfa4242
Francisco - Postman <francisco.deppe@postman.com@Franciscos-MacBook-Pro-3.local>
2022-12-17 11:47:15 -0600

    added email send

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,6 @@
+import requests 
+import os
+from sqlalchemy import or_
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256 
@@ -11,25 +14,46 @@ from flask_jwt_extended import (
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema 
 from blocklist import BLOCKLIST
 
 blp = Blueprint("Users", "users", description="Operations on Users")
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+		f"https://api.mailgun.net/v3/{domain}/messages",
+		auth=("api", os.getenv("MAILGUN_API_KEY")),
+		data={"from": "Francisco <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body})
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"],
+            )
+        ).first():
             abort(409, message="A user with that name already exists")
         
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"])
         )
 
         db.session.add(user)
         db.session.commit()
+        send_simple_message(
+            to=user.email,
+            subject="Successfuly signed up",
+            body=f"Hi {user.username}, you have successfully signed up"
+        )
 
         return {"message": "user created successfully"}, 201
 
