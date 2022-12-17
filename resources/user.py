3cf808529b940f63da644a24afaa535f47134020
Francisco - Postman <francisco.deppe@postman.com@Franciscos-MacBook-Pro-3.local>
2022-12-17 12:53:27 -0600

    updating/fixing files

--- resources/user.py
+++ resources/user.py
@@ -1,26 +1,31 @@
-import requests 
 import os
-from flask import current_app
-from sqlalchemy import or_
+import redis
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from passlib.hash import pbkdf2_sha256 
 from flask_jwt_extended import (
     create_access_token,
-    get_jwt,
     create_refresh_token,
     get_jwt_identity,
+    get_jwt,
     jwt_required,
 )
+from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
+from rq import Queue
 
 from tasks import send_user_registration_email
+
 from db import db
 from models import UserModel
-from schemas import UserSchema, UserRegisterSchema 
+from schemas import UserSchema, UserRegisterSchema
 from blocklist import BLOCKLIST
-from tasks import send_simple_message
 
-blp = Blueprint("Users", "users", description="Operations on Users")
+blp = Blueprint("Users", "users", description="Operations on users")
+connection = redis.from_url(
+    os.getenv("REDIS_URL")
+)  # Get this from Render.com or run in Docker
+queue = Queue("emails", connection=connection)
+
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -32,20 +37,19 @@ class UserRegister(MethodView):
                 UserModel.email == user_data["email"],
             )
         ).first():
-            abort(409, message="A user with that name/email already exists")
-        
+            abort(409, message="A user with that username or email already exists.")
+
         user = UserModel(
             username=user_data["username"],
             email=user_data["email"],
-            password=pbkdf2_sha256.hash(user_data["password"])
+            password=pbkdf2_sha256.hash(user_data["password"]),
         )
-
         db.session.add(user)
         db.session.commit()
-        
-        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
 
-        return {"message": "user created successfully"}, 201
+        queue.enqueue(send_user_registration_email, user.email, user.username)
+
+        return {"message": "User created successfully."}, 201
 
 
 @blp.route("/login")
@@ -61,7 +65,37 @@ class UserLogin(MethodView):
             refresh_token = create_refresh_token(user.id)
             return {"access_token": access_token, "refresh_token": refresh_token}, 200
 
-        abort(401, message="Invalid credentials.") 
+        abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    """
+    This resource can be useful when testing our Flask app.
+    We may not want to expose it to public users, but for the
+    sake of demonstration in this course, it can be useful
+    when we are manipulating data regarding the users.
+    """
+
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200
 
 
 @blp.route("/refresh")
@@ -74,27 +108,3 @@ class TokenRefresh(MethodView):
         jti = get_jwt()["jti"]
         BLOCKLIST.add(jti)
         return {"access_token": new_token}, 200
-         
-@blp.route("/logout")
-class UserLogout(MethodView):
-    @jwt_required()
-    def post(self):
-        jti = get_jwt()["jti"]
-        BLOCKLIST.add(jti)
-        return {"message": "Successfully logged out"}, 200
-
-@blp.route("/user/<int:user_id>")
-class User(MethodView):
-    @blp.response(200, UserSchema)
-    def get(self, user_id):
-        user = UserModel.query.get_or_404(user_id)
-        return user
-
-    def delete(self, user_id):
-        user = UserModel.query.get_or_404(user_id)
-        db.session.delete(user)
-        db.session.commit()
-        return{"message": "user deleted"}, 200
-
-
-
